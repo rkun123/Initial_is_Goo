a9e51b47a87dc15f89289bcd54ad0f6327ea9ebf
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2020-12-19 16:14:05 +0900

    [update] "ホームの完成"

--- frontend/src/views/Host.vue
+++ frontend/src/views/Host.vue
@@ -1,21 +1,51 @@
 <template>
   <div class="user">
-    <h1>This is an Host page</h1>
-    <router-link to="/">Home</router-link><br>
-    <HelloWorld msg="じゃんけん~ルーム作成~"/>
-    <input v-model="message" placeholder="User name"><br>
-    <input v-model="message" placeholder="room name"><br>
-    <link-button path="/roomhost">ルーム作成</link-button><br>
+    <h1>じゃんけん~ルーム作成~</h1>
+    <fm1 class="fmm1" placeholder="Room name"></fm1>
+    <fm2 class="fmm2" placeholder="User neme"></fm2>
+    <div class="room-making">
+      <button class="button-margin" @click="make_room">作成</button>
+    </div>
   </div>
 </template>
 
 <script>
-import LinkButton from '@/components/Link.vue'
+import Form_text from '@/components/Form_text.vue'
 
 export default {
   name: 'Host',
   components: {
-    LinkButton
+    "fm1": Form_text,
+    "fm2": Form_text
   },
+  methods: {
+    make_room(){
+      this.$router.push('/roomhost')
+    }
+  }
 }
 </script>
+
+<style scoped>
+.fmm1 >>> .icon{
+  display:none;
+}
+.fmm2 >>> .icon{
+  display:none;
+}
+.fmm2{
+  margin-top: 25px;
+}
+.button-margin {
+  font-size: 20px;
+  margin-top: 25px;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+  border-bottom-right-radius: 50px;
+  border-bottom-left-radius: 50px;
+  width: 15%;
+  height: 40px;
+  max-width: 200px;
+}
+
+</style>
